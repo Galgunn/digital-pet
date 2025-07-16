3db253d1fb41539e2015c5effcf7aa6b848ed577
Galgunn <Gtzkm18@gmail.com>
2025-07-16 14:46:19 -0400

    Updated dialogue system
    Trying to figure out how to reset variables so that whenever I call the dialogue manager it starts from the top
    
    v 1.9.14

--- game_states/dialog_box.py
+++ game_states/dialog_box.py
@@ -1,5 +1,6 @@
 import pygame
 from scripts.state import State
+from scripts.dialogue_system import DialogueSystem
 
 class DialogBox(State):
     def __init__(self, game):
@@ -9,42 +10,14 @@ class DialogBox(State):
             'another message',
             'and another'
         ]
-
-        self.font = pygame.font.SysFont('engravers', 12)
-        self.snip = self.font.render('', True, (255, 255, 255))
-        self.counter = 0
-        self.speed = 3
-        self.line_done = False
-        self.current_line = 0
-        self.line = self.lines[self.current_line]
-        self.dialogue_complete = False
+        self.dialogue_system = DialogueSystem(self.game, self.lines)
 
     def update(self):
-        if self.game.interaction_options['left click']['just pressed'] and self.dialogue_complete:
-            self.counter = 0
-            self.line_done = False
-            self.current_line = 0
-            self.line = self.lines[self.current_line]
-            self.dialogue_complete = False
+        self.dialogue_system.update()
+        if self.game.interaction_options['left click']['just pressed'] and self.dialogue_system.dialogue_complete:
             self.exit_state()
-        else:
-            if self.counter < self.speed * len(self.line):
-                self.counter += 1
-            elif self.counter >= self.speed * len(self.line):
-                self.line_done = True
-
-            if self.current_line == len(self.lines) - 1 and self.line_done:
-                self.dialogue_complete = True
-            elif self.game.interaction_options['left click']['just pressed'] and not self.line_done: # Display current message instantly
-                self.counter = self.speed * len(self.line)
-            elif self.game.interaction_options['left click']['just pressed'] and self.line_done and self.current_line < len(self.lines) - 1: # Proceed to next line
-                self.current_line += 1
-                self.line_done = False
-                self.line = self.lines[self.current_line]
-                self.counter = 0
-
-        self.snip = self.font.render(self.line[0:self.counter//self.speed], True, 'white')
+            self.dialogue_system.reset()
 
     def render(self, surf):
-        surf.blit('red')
-        surf.blit(self.snip, (10, 10))
+        surf.fill('red')
+        self.dialogue_system.render(surf, (10, 10))
---
+++ scripts/dialogue_system.py
@@ -0,0 +1,44 @@
+import pygame
+from scripts.utils import FONT
+
+class DialogueSystem:
+    def __init__(self, game, lines:list):
+        self.game = game
+        self.dialogue_lines = lines
+        self.font = FONT
+        self.snip = self.font.render('', True, (255, 255, 255))
+        self.counter = 0
+        self.speed = 3
+        self.line_done = False
+        self.current_line = 0
+        self.line = self.dialogue_lines[self.current_line]
+        self.dialogue_complete = False
+
+    def update(self):
+        # Handles the type writer effect
+        if self.counter < self.speed * len(self.line):
+            self.counter += 1
+        elif self.counter >= self.speed * len(self.line):
+            self.line_done = True
+
+        if self.game.interaction_options['left click']['just pressed'] and not self.line_done: # Display current message instantly
+            self.counter = self.speed * len(self.line)
+        elif self.game.interaction_options['left click']['just pressed'] and self.line_done and self.current_line < len(self.dialogue_lines) - 1: # Proceed to next line
+            self.current_line += 1
+            self.line_done = False
+            self.line = self.dialogue_lines[self.current_line]
+            self.counter = 0
+        elif self.current_line == len(self.dialogue_lines) - 1 and self.line_done:
+            self.dialogue_complete = True
+
+        self.snip = self.font.render(self.line[0:self.counter//self.speed], True, 'white')
+
+    def render(self, surf, pos:tuple):
+        surf.blit(self.snip, pos)
+
+    def reset(self):
+        self.counter = 0
+        self.line_done = False
+        self.current_line = 0
+        self.line = self.dialogue_lines[self.current_line]
+        self.dialogue_complete = False
